97d9bb57f2c3ff66ec33b99b905e0b6513824fbd
HovhannesTumanyan <67688341+HovhannesTumanyan@users.noreply.github.com>
2020-09-07 16:39:45 +0400

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	int A[11],B[2];
+	int i,j,k,a=0;
+	bool b=true;
+	for (i = 1; i <= 10; i++) {
+		cin >> A[i];
+	};
+	///
+	/// 
+	/// 
+	for (k = 2; k <= 10; k++) {
+		B[1] = A[k];
+		for (j = k-1; j >= 1; j--) {
+			B[0] = A[j];
+
+			if (B[1] < B[0]) { b = false; break; }
+			
+		};
+		if (b == true) {
+			a++;
+			b = true;
+			
+		}
+	};
+	cout << a;
+
+}
